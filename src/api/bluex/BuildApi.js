6dbb3bd4acfbb0563094b36f9da34fc460002e13
zhangshaobo2020 <zhangshaobo2020@github.com>
2025-08-13 21:21:33 +0800

    部分小优化

--- src/api/bluex/BuildApi.js
+++ src/api/bluex/BuildApi.js
@@ -1,21 +1,5 @@
 import request from '../axios'
 
-export function manuallyTriggeredTest(data) {
-    return request({
-        url: '/build/manuallyTriggeredTest',
-        method: 'post',
-        data: data
-    })
-}
-
-export function fileSystemListenerTest(data) {
-    return request({
-        url: '/build/fileSystemListenerTest',
-        method: 'post',
-        data: data
-    })
-}
-
 export function autoMatchTest(data) {
     return request({
         url: '/build/autoMatchTest',
